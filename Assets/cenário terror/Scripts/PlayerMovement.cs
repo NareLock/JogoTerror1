92d6cac14a41e9492891ceb83c77de4535eccabe
Turith1 <162712980+Turith1@users.noreply.github.com>
2024-07-14 20:57:20 -0300

    Control polishment

--- Assets/cenário terror/Scripts/PlayerMovement.cs
+++ Assets/cenário terror/Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController _controller;
     public CinemachineVirtualCamera _vCCamera;
     private float _speed = 4f;
-    public float _gravity = -9.81f;
+    public float _gravity = -45f;
     public float _x;
     public float _z;
     public float x;
@@ -37,6 +37,18 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Movment();
+        //Drinks Wine for special vision
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Invoke("Drink", 2);
+        }
+        //Throws holy water
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+           Invoke("ThrowWater", 2);
+        }
+        HoldCross();
+        HoldBible();
         if (Input.GetKeyDown(KeyCode.E))
         {
             _vCCamera.enabled = true;
@@ -47,21 +59,55 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    public void Movment()
+    private void Movment()
     {
         _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
-        if (_isGrounded && _velocity.y < 0)
+        if (_isGrounded && Input.GetKeyUp(KeyCode.Space))
         {
-            _velocity.y = -2;
+            _velocity.y = 4;
         }
         else
         {
             x = Input.GetAxis("Horizontal");
             z = Input.GetAxis("Vertical");
         }
-        Vector3 move = (transform.right * Input.GetAxis("Horizontal")) / 2.3f + transform.forward * Input.GetAxis("Vertical");
+        Vector3 move = (transform.right * Input.GetAxisRaw("Horizontal")) / 2.3f + transform.forward * Input.GetAxisRaw("Vertical");
         _controller.Move(_speed * Time.deltaTime * move);
         _velocity.y += _gravity * Time.deltaTime;
         _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    private void Drink()
+    {
+    }
+
+    private void ThrowWater()
+    {
+    }
+
+    private void HoldCross()
+    {
+        //Hold up cross
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+
+        }
+    }
+
+    private void HoldBible()
+    {
+        //Holds up bible
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+
+        }
+        else if (Input.GetKeyUp(KeyCode.G))
+        {
+
+        }
     }
 }
