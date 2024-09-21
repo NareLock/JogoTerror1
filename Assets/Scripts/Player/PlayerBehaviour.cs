a4fedb72ad31dac25be9149ef66b960b629d888f
Turith1 <162712980+Turith1@users.noreply.github.com>
2024-09-20 21:52:32 -0300

    First inheritance script made
    door opens with E key

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -2,11 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+interface IInteractable
+{
+    public void Interact();
+}
 public class PlayerBehaviour : MonoBehaviour
 {
 
-    private string _bTN;
+    Vector3 rayOrigin = new Vector3(0.5f, 0.5f, 0f);
+
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = Camera.main.ViewportPointToRay(rayOrigin);
+            //Ray ray = new Ray(transform.position, transform.forward);
+            Debug.DrawRay(ray.origin, ray.direction * 5, Color.red, 2);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, 5))
+            {
+                if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObject))
+                {
+                    interactObject.Interact();
+                }
+            }
+        }
     }
 }
